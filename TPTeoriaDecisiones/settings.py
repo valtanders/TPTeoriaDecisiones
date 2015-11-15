225454d12b943c4989832e0c793f0dae46935754
valtanders <brunobollati@gmail.com>
2015-11-15 16:48:29 -0300

    creada pagina index y su redireccion

--- TPTeoriaDecisiones/settings.py
+++ TPTeoriaDecisiones/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'TPTeoriaDecisiones.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'TeoriaDeDecisiones/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
