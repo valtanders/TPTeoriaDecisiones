d5d82e1761f4f5c53e20800259784b2fa3971c93
Jorge <jorgeluis.tanos@exitco.biz>
2015-11-15 19:58:40 -0300

    cache

--- TeoriaDeDecisiones/views.py
+++ TeoriaDeDecisiones/views.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, render_to_response
-
+from django.core.cache import cache
 
 # Create your views here.
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from TeoriaDeDecisiones.models import Lista
 
-lista = Lista()
+
+listaAlter = []
+listaConse = []
 
 @csrf_exempt
 def nuevo_usuario(request):
@@ -50,7 +52,7 @@ def user_login(request, user=None):
                 login(request, user)
                 if user.is_staff:
                     return HttpResponseRedirect('admin')
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse('principal'))
                 # Redirect to a success page.
             else:
                 error = 'Cuenta desactivada'
@@ -73,3 +75,27 @@ def index(request):
 def user_logout(request):
     logout(request)
     return render_to_response('index.html')
+
+
+@csrf_exempt
+def redirect_principal(request):
+    return render_to_response('principal.html', context_instance=RequestContext(request))
+
+
+@csrf_exempt
+def ingresar_datos(request):
+    if request.method == "POST":
+        if request.POST["txt_alternativa"] != "":
+            listaAlter.append(request.POST['txt_alternativa'])
+            i = 1
+        else:
+            listaConse.append(request.POST['txt_consecuencia'])
+
+    return render_to_response('principal.html', {'user': request.user, 'alt': listaAlter, 'con': listaConse})
+
+
+
+
+@csrf_exempt
+def calcula_datos(request):
+    a = "hola"
