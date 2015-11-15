29ad54376b4f9ed329f59cda7a2a0f85d87dc120
Jorge <jorgeluis.tanos@exitco.biz>
2015-11-15 17:11:30 -0300

    modelos

--- TeoriaDeDecisiones/models.py
+++ TeoriaDeDecisiones/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Alternativas(models.Model):
+    texto = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Consecuencias(models.Model):
+    texto = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
